e68583b36c6e1965e8b80230aeee1a88fa198dd5
Oussama Touhami <touhami200622@gmail.com>
2025-07-04 20:34:57 +0000

    Update websiteContent.js

--- src/websiteContent.js
+++ src/websiteContent.js
@@ -9,132 +9,153 @@ let navData = [
     { href: "#catalogue", label: "Catalogue" },
     { href: "#products", label: "Products" },
     { href: "#about", label: "About us" },
-    { href: "#contact", label: "Contact" }
-];
+    { href: "#contact", label: "Contact" },
+]
 
 let homeContent = {
     title: "Fragrance",
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, tempore!,dipisicing elit. Ipsa, tempore!",
     button: "Discover",
-    homeImg: "bkcollection/images/homeImg.png",
+    homeImg: "./public/images/homeImg.png",
     imgAlt: "home Image"
-};
+}
 
 let offersImages = [
-    { imageUrl: "bkcollection/images/offer1.jpg" },
-    { imageUrl: "bkcollection/images/offer2.jpg" },
-    { imageUrl: "bkcollection/images/offer3.jpg" }
-];
+    { imageUrl: "./public/images/offer1.jpg" },
+    { imageUrl: "./public/images/offer2.jpg" },
+    { imageUrl: "./public/images/offer3.jpg" },
+]
 
-let prductPageContent = {
+let prductPageContent = { 
     title: "All products",
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit!",
-};
+}
 
-let prductPageButtons = [
+let prductPageButtons = [ 
     { button: "New products" },
     { button: "Men's collection" },
     { button: "Women's collection" },
-];
+]
 
-let products = {
+let products = { 
     allProducts: [
         {
-            productImage: "bkcollection/images/perf2.png",
+            productImage: "./public/images/perf2.png",
             productTitle: "Product 1",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "desc 1 Lorem ipsum..."
+            productDescription: "desc 1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "bkcollection/images/ar1.png",
+            productImage: "./public/images/ar1.png",
             productTitle: "Product 2",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit..."
         },
         {
-            productImage: "bkcollection/images/perf1.png",
+            productImage: "./public/images/perf1.png",
             productTitle: "Product 3",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit..."
         },
         {
-            productImage: "bkcollection/images/ar2.png",
+            productImage: "./public/images/ar2.png",
             productTitle: "Product 4",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit..."
         }
     ],
     mensCollection: [
         {
-            productImage: "bkcollection/images/perf2.png",
+            productImage: "./public/images/perf2.png",
             productTitle: "Product 1",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
         },
         {
-            productImage: "bkcollection/images/perf2.png",
+            productImage: "./public/images/perf2.png",
             productTitle: "Product 2",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
         },
         {
-            productImage: "bkcollection/images/perf2.png",
+            productImage: "./public/images/perf2.png",
             productTitle: "Product 3",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
         },
         {
-            productImage: "bkcollection/images/perf2.png",
+            productImage: "./public/images/perf2.png",
             productTitle: "Product 4",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
         }
     ],
     womensCollection: [
         {
-            productImage: "bkcollection/images/ar2.png",
+            productImage: "./public/images/ar2.png",
             productTitle: "Product 1",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
         },
         {
-            productImage: "bkcollection/images/ar3.png",
+            productImage: "./public/images/ar3.png",
             productTitle: "Product 2",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
         },
         {
-            productImage: "bkcollection/images/ar1.png",
+            productImage: "./public/images/ar1.png",
             productTitle: "Product 3",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
         },
         {
-            productImage: "bkcollection/images/perf1.png",
+            productImage: "./public/images/perf1.png",
             productTitle: "Product 4",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
         }
     ],
     newProducts: [
         {
-            productImage: "bkcollection/images/ar1.png",
+            productImage: "./public/images/ar1.png",
             productTitle: "ROUGE",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum..."
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
         },
         {
-            productImage: "bkcollection/images/ar2.png",
+            productImage: "./public/images/ar2.png",
+            productTitle: "BLEU",
+            productPrice: "29$",
+            productRating: "⭐⭐⭐⭐",
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+        },
+        {
+            productImage: "./public/images/ar3.png",
+            productTitle: "ROSE",
+            productPrice: "29$",
+            productRating: "⭐⭐⭐⭐⭐",
+            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+        },
+    ],
+};
+
+let newArrivalsContent = {
+    title: "New Arrivals",
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit consectetur adipisicing elit!",
+}
+
+export { navData, logo, homeContent, offersImages, prductPageContent, prductPageButtons, products, newArrivalsContent }
