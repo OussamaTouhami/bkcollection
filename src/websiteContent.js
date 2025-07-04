9ffdba61e23323e75ad821feef1ba198c4da99c9
Oussama Touhami <touhami200622@gmail.com>
2025-07-04 20:39:08 +0000

    Update websiteContent.js

--- src/websiteContent.js
+++ src/websiteContent.js
@@ -5,151 +5,169 @@ let logo = {
 }
 
 let navData = [
-    { href: "#home", label: "Home" },
-    { href: "#catalogue", label: "Catalogue" },
-    { href: "#products", label: "Products" },
-    { href: "#about", label: "About us" },
-    { href: "#contact", label: "Contact" },
+    {
+        href: "#home",
+        label: "Home"
+    },
+    {
+        href: "#catalogue",
+        label: "Catalogue"
+    },
+    {
+        href: "#products",
+        label: "Products"
+    },
+    {
+        href: "#about",
+        label: "About us"
+    },
+    {
+        href: "#contact",
+        label: "Contact"
+    },
 ]
-
 let homeContent = {
     title: "Fragrance",
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa, tempore!,dipisicing elit. Ipsa, tempore!",
     button: "Discover",
-    homeImg: "./public/images/homeImg.png",
+    homeImg: "/bkcollection/images/homeImg.png",
     imgAlt: "home Image"
 }
-
 let offersImages = [
-    { imageUrl: "./public/images/offer1.jpg" },
-    { imageUrl: "./public/images/offer2.jpg" },
-    { imageUrl: "./public/images/offer3.jpg" },
+    {
+        imageUrl: "/bkcollection/images/offer1.jpg"
+    },
+    {
+        imageUrl: "/bkcollection/images/offer2.jpg"
+    },
+    {
+        imageUrl: "/bkcollection/images/offer3.jpg"
+    },
 ]
-
 let prductPageContent = { 
     title: "All products",
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit!",
+    
 }
-
 let prductPageButtons = [ 
-    { button: "New products" },
-    { button: "Men's collection" },
-    { button: "Women's collection" },
+    {button: "New products"},
+    {button: "Men's collection"},
+    {button: "Women's collection"},
 ]
-
 let products = { 
     allProducts: [
         {
-            productImage: "./public/images/perf2.png",
+            productImage: "/bkcollection/images/perf2.png",
             productTitle: "Product 1",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "desc 1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
+            productDescription: " desc 1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/ar1.png",
+            productImage: "/bkcollection/images/ar1.png",
             productTitle: "Product 2",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐",
-            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/perf1.png",
+            productImage: "/bkcollection/images/perf1.png",
             productTitle: "Product 3",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/ar2.png",
+            productImage: "/bkcollection/images/ar2.png",
             productTitle: "Product 4",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         }
     ],
     mensCollection: [
         {
-            productImage: "./public/images/perf2.png",
+            productImage: "/bkcollection/images/perf2.png",
             productTitle: "Product 1",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/perf2.png",
+            productImage: "/bkcollection/images/perf2.png",
             productTitle: "Product 2",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/perf2.png",
+            productImage: "/bkcollection/images/perf2.png",
             productTitle: "Product 3",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/perf2.png",
+            productImage: "/bkcollection/images/perf2.png",
             productTitle: "Product 4",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         }
     ],
     womensCollection: [
         {
-            productImage: "./public/images/ar2.png",
+            productImage: "/bkcollection/images/ar2.png",
             productTitle: "Product 1",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/ar3.png",
+            productImage: "/bkcollection/images/ar3.png",
             productTitle: "Product 2",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/ar1.png",
+            productImage: "/bkcollection/images/ar1.png",
             productTitle: "Product 3",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/perf1.png",
+            productImage: "/bkcollection/images/perf1.png",
             productTitle: "Product 4",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         }
     ],
     newProducts: [
         {
-            productImage: "./public/images/ar1.png",
+            productImage: "/bkcollection/images/ar1.png",
             productTitle: "ROUGE",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/ar2.png",
+            productImage: "/bkcollection/images/ar2.png",
             productTitle: "BLEU",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
         {
-            productImage: "./public/images/ar3.png",
+            productImage: "/bkcollection/images/ar3.png",
             productTitle: "ROSE",
             productPrice: "29$",
             productRating: "⭐⭐⭐⭐⭐",
-            productDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit..."
+            productDescription: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse accusamus facilis voluptas deleniti fugiat ut harum neque atque impedit eum iure labore, quod est veniam obcaecati odio! Et, aspernatur eveniet."
         },
+        
     ],
 };
 
@@ -158,4 +176,4 @@ let newArrivalsContent = {
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit consectetur adipisicing elit!",
 }
 
-export { navData, logo, homeContent, offersImages, prductPageContent, prductPageButtons, products, newArrivalsContent }
+export { navData,logo,homeContent, offersImages ,prductPageContent , prductPageButtons,products,newArrivalsContent}
